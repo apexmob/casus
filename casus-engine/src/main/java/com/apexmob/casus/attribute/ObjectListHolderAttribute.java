50ae6f638b21c14a2309b49e938f933c178c408e
Chris Kirk <kirksc1@gmail.com>
2018-10-03 23:32:09 -0500

    add javadoc

--- casus-engine/src/main/java/com/apexmob/casus/attribute/ObjectListHolderAttribute.java
+++ casus-engine/src/main/java/com/apexmob/casus/attribute/ObjectListHolderAttribute.java
@@ -4,15 +4,45 @@ import com.apexmob.casus.GameObjectAttribute;
 
 import java.util.List;
 
+/**
+ * ObjectListHolderAttribute represents the basic interface for attributes that hold a list of objects.
+ *
+ * @author Chris Kirk
+ * @since 1.0
+ * @param <T> The type of object held by the attribute.
+ */
 public interface ObjectListHolderAttribute<T> extends GameObjectAttribute {
 
+    /**
+     * Retrieve the list of objects held within the attribute.
+     * @return The list of objects.
+     */
     List<T> getObjects();
 
+    /**
+     * Remove an object from the list.
+     * @param obj The object to remove.
+     * @return True if the object was present and removed, otherwise false.
+     */
     boolean removeObject(T obj);
 
+    /**
+     * Add an object to the list.
+     * @param obj The object to add.
+     * @return True if the object was added, otherwise false.
+     */
     boolean addObject(T obj);
 
+    /**
+     * Add an event listener.
+     * @param listener The listener to add.
+     */
     void addObjectListChangeListener(ObjectListChangeListener<T> listener);
 
+    /**
+     * Remove an event listener.
+     * @param listener The listener to remove.
+     * @return True if the listener was present and removed, otherwise false.
+     */
     boolean removeObjectListChangeListener(ObjectListChangeListener<T> listener);
 }
--- casus-engine/src/main/java/com/apexmob/casus/attribute/SimpleObjectListHolder.java
+++ casus-engine/src/main/java/com/apexmob/casus/attribute/SimpleObjectListHolder.java
@@ -8,16 +8,29 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * SimpleObjectListHolder provides a simple implementation of {@link ObjectListHolderAttribute}.
+ *
+ * @author Chris Kirk
+ * @since 1.0
+ * @param <T> The type of object held by the attribute.
+ */
 public class SimpleObjectListHolder<T> extends AbstractGameObjectAttribute implements ObjectListHolderAttribute<T> {
 
     private final List<T> objects = new ArrayList<>();
     private final List<ObjectListChangeListener> listeners = new ArrayList<>();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<T> getObjects() {
         return Collections.unmodifiableList(objects);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean removeObject(T obj) {
         boolean retVal = objects.remove(obj);
@@ -30,6 +43,9 @@ public class SimpleObjectListHolder<T> extends AbstractGameObjectAttribute imple
         return retVal;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean addObject(T obj) {
         boolean retVal = objects.add(obj);
@@ -41,11 +57,17 @@ public class SimpleObjectListHolder<T> extends AbstractGameObjectAttribute imple
         return retVal;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void addObjectListChangeListener(ObjectListChangeListener<T> listener) {
         this.listeners.add(listener);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean removeObjectListChangeListener(ObjectListChangeListener<T> listener) {
         return listeners.remove(listener);
